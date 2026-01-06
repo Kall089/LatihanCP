f520041a2f747d72191610292bc1e008c1e920aa
Kall089 <haikalmuzhoffar@gmail.com>
2026-01-06 19:11:04 +0700

    uniqueNumber

---
+++ LatihanCP/bitManipulationProblem/uniqueNumber.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int uniqueNo1(vector<int> arr)
+{
+    int n = arr.size();
+
+    int cumulative_xor = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+
+        cumulative_xor = cumulative_xor ^ arr[i];
+    }
+
+    return cumulative_xor;
+}
+
+int main()
+{
+    vector<int> arr = {1, 3, 5, 4, 3, 1, 5};
+
+    cout << uniqueNo1(arr) << endl;
+}
