344dadd15b12b6a23ae199049bd8f359ea33bec4
Kall089 <haikalmuzhoffar@gmail.com>
2026-06-03 12:12:12 +0700

    done

---
+++ SocialMediaQt/build/CMakeFiles/SocialMediaQt.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "SocialMediaQt_autogen/timestamp" "custom" "SocialMediaQt_autogen/deps"
+  "/Users/haikalmuzhoffar/KULIAH/SocialMediaQt/Lorem-Ipsum/MainWindow.cpp" "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/MainWindow.cpp.o" "gcc" "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/MainWindow.cpp.o.d"
+  "/Users/haikalmuzhoffar/KULIAH/SocialMediaQt/Lorem-Ipsum/Notifikasi.cpp" "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/Notifikasi.cpp.o" "gcc" "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/Notifikasi.cpp.o.d"
+  "/Users/haikalmuzhoffar/KULIAH/SocialMediaQt/Lorem-Ipsum/PostWidget.cpp" "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/PostWidget.cpp.o" "gcc" "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/PostWidget.cpp.o.d"
+  "/Users/haikalmuzhoffar/KULIAH/SocialMediaQt/Lorem-Ipsum/SocialMedia.cpp" "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/SocialMedia.cpp.o" "gcc" "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/SocialMedia.cpp.o.d"
+  "/Users/haikalmuzhoffar/KULIAH/SocialMediaQt/Lorem-Ipsum/chat.cpp" "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/chat.cpp.o" "gcc" "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/chat.cpp.o.d"
+  "/Users/haikalmuzhoffar/KULIAH/SocialMediaQt/Lorem-Ipsum/feed.cpp" "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/feed.cpp.o" "gcc" "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/feed.cpp.o.d"
+  "/Users/haikalmuzhoffar/KULIAH/SocialMediaQt/Lorem-Ipsum/follow.cpp" "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/follow.cpp.o" "gcc" "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/follow.cpp.o.d"
+  "/Users/haikalmuzhoffar/KULIAH/SocialMediaQt/Lorem-Ipsum/like.cpp" "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/like.cpp.o" "gcc" "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/like.cpp.o.d"
+  "/Users/haikalmuzhoffar/KULIAH/SocialMediaQt/Lorem-Ipsum/main.cpp" "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/main.cpp.o" "gcc" "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/main.cpp.o.d"
+  "/Users/haikalmuzhoffar/KULIAH/SocialMediaQt/Lorem-Ipsum/trending.cpp" "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/trending.cpp.o" "gcc" "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/trending.cpp.o.d"
+  "/Users/haikalmuzhoffar/KULIAH/SocialMediaQt/build/SocialMediaQt_autogen/mocs_compilation.cpp" "CMakeFiles/SocialMediaQt.dir/SocialMediaQt_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/SocialMediaQt.dir/SocialMediaQt_autogen/mocs_compilation.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ SocialMediaQt/build/CMakeFiles/SocialMediaQt.dir/cmake_clean.cmake
@@ -0,0 +1,36 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SocialMediaQt_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/SocialMediaQt_autogen.dir/ParseCache.txt"
+  "SocialMediaQt_autogen"
+  "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/MainWindow.cpp.o"
+  "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/MainWindow.cpp.o.d"
+  "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/Notifikasi.cpp.o"
+  "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/Notifikasi.cpp.o.d"
+  "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/PostWidget.cpp.o"
+  "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/PostWidget.cpp.o.d"
+  "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/SocialMedia.cpp.o"
+  "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/SocialMedia.cpp.o.d"
+  "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/chat.cpp.o"
+  "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/chat.cpp.o.d"
+  "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/feed.cpp.o"
+  "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/feed.cpp.o.d"
+  "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/follow.cpp.o"
+  "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/follow.cpp.o.d"
+  "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/like.cpp.o"
+  "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/like.cpp.o.d"
+  "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/main.cpp.o"
+  "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/main.cpp.o.d"
+  "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/trending.cpp.o"
+  "CMakeFiles/SocialMediaQt.dir/Lorem-Ipsum/trending.cpp.o.d"
+  "CMakeFiles/SocialMediaQt.dir/SocialMediaQt_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/SocialMediaQt.dir/SocialMediaQt_autogen/mocs_compilation.cpp.o.d"
+  "SocialMediaQt"
+  "SocialMediaQt.pdb"
+  "SocialMediaQt_autogen/mocs_compilation.cpp"
+  "SocialMediaQt_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SocialMediaQt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
